978df9a04f0fb9901c51d71fa0bf80335d5c6b99
Craig Fisher <bzc@hotmail.com>
2020-05-07 23:19:53 -0700

    Updates

---
+++ precache-manifest.dbc5231ac5ea24d7136b12ee17af0acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c8421bfba2205614d5cf8f3bb548f3",
+    "url": "/SkullKingScorer/index.html"
+  },
+  {
+    "revision": "f23a80cb4ff746952561",
+    "url": "/SkullKingScorer/static/css/main.568c822e.chunk.css"
+  },
+  {
+    "revision": "a28929858727d7322786",
+    "url": "/SkullKingScorer/static/js/2.ac6b0938.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SkullKingScorer/static/js/2.ac6b0938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23a80cb4ff746952561",
+    "url": "/SkullKingScorer/static/js/main.5f0ffb0e.chunk.js"
+  },
+  {
+    "revision": "4b9f1673c352338940ac",
+    "url": "/SkullKingScorer/static/js/runtime-main.396f35eb.js"
+  }
+]);
